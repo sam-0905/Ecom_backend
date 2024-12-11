03ba4627d1c289c744fd2d7abdc7fdc73df4c467
Sam <118877482+sam-0905@users.noreply.github.com>
2024-12-11 13:12:13 +0530

    created  contex,reducer,cart for cart logic

---
+++ Ecommerce_backend/src/Context/Cart-context.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+
+const CartContext = createContext()
+
+const CartProvider = (children) => {
+    
+    return <CartContext.Provider>{children}</CartContext.Provider>
+
+}
+
+const useCart = useContext(CartContext) 

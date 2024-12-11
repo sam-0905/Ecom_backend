556a6867d5904fb043be0a22a6d504fc557a14e3
Sam <118877482+sam-0905@users.noreply.github.com>
2024-12-11 13:13:13 +0530

    updated home

--- Ecommerce_backend/src/Pages/Home.jsx
+++ Ecommerce_backend/src/Pages/Home.jsx
@@ -1,6 +1,17 @@
+import {useState,useEffect } from "react";
 import homePage from "../assets/ProductImg/homePage.png"
+import axios from "axios";
 
 const Home = () => {
+    const [category,setCategory] = useState([]) 
+
+    useEffect(() => {
+        axios.get("/api/categories")
+        .then((res) => {
+        setCategory(res.data.categories)
+        console.log(res.data.categories)
+        })
+    })
 
     return (
     <>
@@ -17,6 +28,17 @@ const Home = () => {
             <h1>Home</h1>
             <button>Shop</button>
             </div>
+        </div>
+        <div>
+            {category && category.map(({categoryName,_id,image}) => (
+               <div className="home-category">
+                 <li key={_id} className="home-cateList">
+                 <img className="home-cateImg" src={image} width="100%" height="auto" alt={categoryName}  c/> 
+                <h1>{categoryName}</h1>
+                </li>
+               </div>
+            ))}
+            
         </div>
     </>
 )

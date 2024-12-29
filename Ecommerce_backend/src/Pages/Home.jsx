83f528c4bb599d9c7a2d4cf55a2e236b3be349ff
Sam <118877482+sam-0905@users.noreply.github.com>
2024-12-29 19:05:51 +0530

    updated

--- Ecommerce_backend/src/Pages/Home.jsx
+++ Ecommerce_backend/src/Pages/Home.jsx
@@ -1,57 +1,67 @@
-import {useState,useEffect, use } from "react";
-import homePage from "../assets/ProductImg/homePage.png"
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import homePage from "../assets/ProductImg/homePage.png";
 import axios from "axios";
-import { useNavigate } from "react-router";
 
 const Home = () => {
-    const [category,setCategory] = useState([])   
-    const navigate = useNavigate()
+  const [category, setCategory] = useState([]);
 
-    useEffect(() => {
-            axios.get("/api/categories")
-            .then((res) => {
-            setCategory(res.data.categories)
-            console.log(res.data.categories)
-            })
-            .catch((err) =>{
-             console.log(err)
-            })
-    },[])    
+  // Fetch categories
+  useEffect(() => {
+    axios
+      .get("/api/categories")
+      .then((res) => {
+        setCategory(res.data.categories);
+        console.log(res.data.categories);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    const handleCategoryClick = (categoryId) => {
-        // Navigate to ProductsPage and pass categoryId as state
-        navigate("/products", { state: { categoryId } });
-      };
-
-
-    return (
+  return (
     <>
-     <div style={{backgroundImage:`url(${homePage})`,  
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',    
-            backgroundRepeat: 'no-repeat',
-            height:"70vh",
-            width:"auto",
-            marginTop:"0",
-            marginBottom:"0"
-            }}>
-            <div className='home'>
-            <h1>Home</h1>
-            <button>Shop</button>
-            </div>
+      {/* Background Section */}
+      <div
+        style={{
+          backgroundImage: `url(${homePage})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "70vh",
+          width: "auto",
+          marginTop: "0",
+          marginBottom: "0",
+        }}
+      >
+        <div className="home">
+          <h1>Home</h1>
+          <button>Shop</button>
         </div>
-        <div>
-            {category && category.map(({categoryName,_id,image}) => (
-               <div className="home-category">
-               <li key={_id} className="home-cateList" onClick={() =>handleCategoryClick(_id)}>
-                <img className="home-cateImg" src={image} width="100%" height="auto" alt={categoryName}/> 
+      </div>
+
+      {/* Categories Section */}
+      <div>
+        {category.map(({ categoryName, _id, image }) => (
+          <div key={_id} className="home-category">
+            <li className="home-cateList">
+              {/* Link to the category page using categoryName */}
+              <Link to={`/category/${categoryName}`}>
+                <img
+                  className="home-cateImg"
+                  src={image}
+                  width="100%"
+                  height="auto"
+                  alt={categoryName}
+                />
                 <h1>{categoryName}</h1>
-                </li>
-               </div>
-            ))}
-            
-        </div>
+              </Link>
+            </li>
+          </div>
+        ))}
+      </div>
     </>
-)
-}
+  );
+};
+
 export default Home;

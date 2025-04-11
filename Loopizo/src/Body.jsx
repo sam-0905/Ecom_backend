52a4c72fd67620ad7e48673cf4a114b8d5a93cfa
Sam <118877482+sam-0905@users.noreply.github.com>
2025-04-11 08:26:54 +0530

    updated the app name

--- Loopizo/src/Body.jsx
+++ Loopizo/src/Body.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
 import axios from 'axios';
+import Filters from "./Pages/Filters";
 
 const Body = () => {
     // const {state, dispatch } = useCart();
@@ -35,6 +36,8 @@ const Body = () => {
     }, [])
 
     return <>
+            <Filters/>
+
             <div className="search-container">
                 <input 
                 type="text"

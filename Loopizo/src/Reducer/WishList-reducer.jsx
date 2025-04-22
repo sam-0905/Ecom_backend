d419b85c78efd4b6667ee879f8e13b917bf0c54d
Sam <118877482+sam-0905@users.noreply.github.com>
2025-04-22 22:08:51 +0530

    updated wishlist component with context and reducer

--- Loopizo/src/Reducer/WishList-reducer.jsx
+++ Loopizo/src/Reducer/WishList-reducer.jsx
@@ -1,33 +1,27 @@
-// ../Reducer/WishList-reducer.jsx
 export const wishRedFunc = (state, action) => {
-    switch (action.type) {
-        case "SET_WISH":
-            return {
-              ...state,
-              wishlist: action.payload.map(item => ({
-                ...item,
-                id: item.id
-              }))
-            };
-          
-      case "ADD_TO_WISHLIST":
-        return {
-          ...state,
-          wishlist: Array.isArray(state.wishlist)
-            ? [...state.wishlist, action.payload]
-            : [action.payload],
-        };
-  
-      case "REMOVE_FROM_WISHLIST":
-        return {
-          ...state,
-          wishlist: Array.isArray(state.wishlist)
-            ? state.wishlist.filter(item => item.id !== action.payload)
-            : [],
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
+  switch (action.type) {
+    case "SET_WISH":
+      return {
+        ...state,
+        wishlist: action.payload.map(item => ({
+          id: item.id,          // needed for delete
+          ...item.product         // spread product info
+        })),
+      };
+    
+    case "ADD_TO_WISHLIST":
+      return {
+        ...state,
+        wishlist: [...state.wishlist, action.payload],
+      };
+
+    case "REMOVE_FROM_WISHLIST":
+      return {
+        ...state,
+        wishlist: state.wishlist.filter(item => item.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};

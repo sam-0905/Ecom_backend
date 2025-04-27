670a7b3b77ecb26bb4f0a48afedae122d6339677
Sam <118877482+sam-0905@users.noreply.github.com>
2025-04-27 23:23:12 +0530

    updated login and email button

--- Loopizo/src/Pages/Login.jsx
+++ Loopizo/src/Pages/Login.jsx
@@ -1,16 +1,18 @@
 import { use, useState } from "react";
 import { useNavigate } from "react-router";
 import Signup from './Signup';
+import { useAuth } from "../Context/Auth-context";
 
 const Login = () =>{
-
+    const {login,setLogin} = useAuth()
+    const [email,setEmail] = useState("")
     const [password,setPassword] = useState("");
     const [error,setError] = useState(false);
-
     const navigate = useNavigate()
 
     function handleLogin() {
         if (password === "4242"){
+            setLogin(true)
             navigate("/Signup")
         }else{
             setPassword("");
@@ -18,13 +20,15 @@ const Login = () =>{
         }
     }
 
-    const onChangeHandler = (e) => setPassword(e.target.value)
-    // console.log(onChangeHandler)
+    const onChangeHandler = (e) => setPassword(e.target.value);
+    const onchangeHandler2 = (e) => setEmail(e.target.value)
+
     return (
         <>  
             <h1>Login page</h1>
             <div>
                 <p>Enter your password</p>
+                <input placeholder="email" onChange={onchangeHandler2} />
                 <input type="password" value={password} onChange={onChangeHandler} />
                 <button onClick={handleLogin}>Login</button>
                 {error && <div> please enter the correct password</div>}

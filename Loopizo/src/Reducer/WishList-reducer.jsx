35612f1999ec78515f4073b9ed3c4c10fe725b60
Sam <118877482+sam-0905@users.noreply.github.com>
2025-04-29 19:04:12 +0530

    updated wish context and reducer

--- Loopizo/src/Reducer/WishList-reducer.jsx
+++ Loopizo/src/Reducer/WishList-reducer.jsx
@@ -2,9 +2,10 @@ export const wishReducer = (state, action) => {
   switch (action.type) {
     case "SET_WISH":
   return action.payload.map(item => ({ ...item, id: item.id }));
-
-    case "ADD_TO_WISHLIST":
-      return [...state, action.payload];
+  
+  case "ADD_TO_WISHLIST":
+    const existingItem = state.find(item => item.id === action.payload.id);
+    return existingItem ? state : [...state, action.payload];
 
     case "REMOVE_FROM_WISHLIST":
       return state.filter(item => item.id !== action.payload); 

e92cf822277afbf52ab727a2767922a24cf6af35
Sam <118877482+sam-0905@users.noreply.github.com>
2025-04-29 22:44:29 +0530

    updated empty wish

--- Loopizo/src/Pages/WishList.jsx
+++ Loopizo/src/Pages/WishList.jsx
@@ -1,18 +1,23 @@
 // import emptyWish from "../assets/icons/emptyWish.png";
 // import "../wishList/wish.css";
-import axios from "axios";
 import { useWish } from "../Context/Wish-context";
+import { useAlert } from "../Context/Alert-context";
+import Alert from "../components/Alert";
+import { useCart } from "../Context/Cart-context";
+import EmptyWish from "../assets/Icon/EmptyWish.png"
 
 const WishList = () => {
-  const { wishState, removeFromWishlist } = useWish();
+  const { wishState, wishDispatch } = useWish();
+  const {showAlert,alert, hideAlert} = useAlert();
+  const {dispatch} = useCart()
 
   return (
     <>
       {wishState.length === 0 ? (
-        <h1 className="empty-wish animate__animated animate__flash">
-          <img src={""} alt="wish-card" />
-          Your wishList is empty
-        </h1>
+          <div className=" animate__animated animate__rubberBand">
+          <img src={EmptyWish} alt="wish-card"className="empty-wish" />
+          <h2 className="empty-wish-text">🛍️ Nothing here yet... go find something you love!</h2>
+          </div>
       ) : (
         wishState.map(({ title, image, id, price }) => (
           console.log("from wishlist",title, image, id, price),
@@ -25,16 +30,44 @@ const WishList = () => {
               <p className="price">Price: ${price}</p>
             </div>
             <div>
-              <button
-                className="button-32"
-                onClick={() => removeFromWishlist(title, image, id, price)}
+
+            <button className="button-56"onClick={() =>{
+                       wishDispatch({ 
+                        type: "REMOVE_FROM_WISHLIST", 
+                        payload: id 
+                      })
+
+                       dispatch({
+                        type: "ADD_TO_CART",
+                        payload: { price, id, title, image },
+                      });
+                      showAlert("Moved to Cart!", "success");
+                }}> Move TO CART
+                </button>
+
+            <button
+              className= "button-32"
+                style={{marginTop:"1rem" ,width:"30%"}}
+                onClick={() =>{
+               wishDispatch({ type: "REMOVE_FROM_WISHLIST", payload: id })
+                 showAlert("Deleted from wishlist", "error");
+                }
+              }
               >
                 <i className="fa fa-trash-o" style={{ fontSize: "16px" }}></i>
               </button>
             </div>
           </div>
+          
         ))
       )}
+       {alert && (
+                     <Alert
+                       message={alert.message}
+                       type={alert.type}
+                       onClose={hideAlert}
+                      />
+                     )}
     </>
   );
 };

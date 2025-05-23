c507b9590453325d92c378e9a865c9582abd23aa
Sam <118877482+sam-0905@users.noreply.github.com>
2025-05-23 13:43:59 +0530

    updated

--- Loopizo/src/components/Header.jsx
+++ Loopizo/src/components/Header.jsx
@@ -1,4 +1,3 @@
-//Nav link
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../Context/Auth-context';
 import { useAlert } from '../Context/Alert-context';
@@ -11,56 +10,54 @@ const getActiveLink = ({ isActive, isPending }) => ({
 });
 
 export const Title = () => (
-    <div>
-      <a href="/" className='logo-container'>
-        <span className='logo-heading'>Loopizo</span>
-      </a>
-         
-    </div>
-  );
+  <div>
+    <a href="/" className='logo-container'>
+      <span className='logo-heading'>Loopizo</span>
+    </a>
+  </div>
+);
 
-const Header = () => {    
-    const {isLoggedIn,setIsLoggedIn} = useAuth();
-    const {showAlert} = useAlert();
-    const {state} = useCart()
-    const {wishState} = useWish()
+const Header = () => {
+  const { isLoggedIn} = useAuth();
+  const { showAlert } = useAlert();
+  const { state } = useCart();
+  const { wishState } = useWish();
 
-   return (
-    <>   
+
+  return (
     <div className="header">
-         <Title/>
-    <nav className="Nav link ">
+      <Title />
+      <nav className="Nav link">
         <NavLink style={getActiveLink} to="/products">
-        <span className="fa fa-product-hunt container"> Products  </span>
+          <span className="fa fa-product-hunt container"> Products </span>
         </NavLink>
         <NavLink style={getActiveLink} to="/wish">
-        {wishState.length > 0 && <span className="badge-wish">{wishState.length}</span>}
-        <span className="fa fa-heart container"> Wishlist</span>
+          <span className="fa fa-heart container"> Wishlist</span>
+          { wishState?.length > 0 && (
+            <span className="badge-wish">{wishState.length}</span>
+          )}
         </NavLink>
         <NavLink style={getActiveLink} to="/cart">
-           {state.quantity > 0 && (
+          <span className="fa fa-cart-arrow-down container"> Cart</span>
+          { state?.quantity > 0 && (
             <span className="badge">{state.quantity}</span>
-          )}                
-           <span className="fa fa-cart-arrow-down container"> Cart</span>
-
+          )}
         </NavLink>
-        <NavLink style={getActiveLink} to="/login" 
-        onClick={() =>{ {
-          setIsLoggedIn((prev) => !prev)
-          showAlert("Logged out successfully!", "error")
-        }
-      }}
-        >
-        <span className="fa fa-sign-in container">  {isLoggedIn ? "Logout" : "Login"} </span>
-        </NavLink>
-
+        {isLoggedIn ? (
+          <NavLink style={getActiveLink} to="/login" onClick={handleLogout}>
+            <span className="fa fa-sign-in container"> Logout </span>
+          </NavLink>
+        ) : (
+          <NavLink style={getActiveLink} to="/login">
+            <span className="fa fa-sign-in container"> Login </span>
+          </NavLink>
+        )}
         <NavLink style={getActiveLink} to="/address">
-        <span className="fa fa-cart-arrow-down container"> address</span>
+          <span className="fa fa-cart-arrow-down container"> Address</span>
         </NavLink>
       </nav>
     </div>
-    </>
-   )
-}
+  );
+};
 
 export default Header;
